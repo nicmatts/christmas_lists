af2a4bc62ab0ea80b604a2fd5a1b4165fb549350
Nicole M <nicmatts@gmail.com>
2014-11-26 12:13:36 -0600

    Remove migration

--- db/migrate/20141125184253_update_users.rb
+++
@@ -1,6 +0,0 @@
-class UpdateUsers < ActiveRecord::Migration
-  def change
-    add_column "users", "username", :string, :limit => 25
-    add_column "users", "password", :string, :limit =>12
-  end
-end
