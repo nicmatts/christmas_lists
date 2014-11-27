3fa6492aa75eaef3aadbf02e5394e360187f1ab7
Nicole M <nicmatts@gmail.com>
2014-11-26 20:34:21 -0600

    Redirect to gift index on login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,7 +23,7 @@ class UserController < ApplicationController
       session[:user_id] = authorized_user.id
       session[:username] = authorized_user.username
       flash[:notice] = "You are now logged in."
-      redirect_to(:controller =>"person", :action => "index")
+      redirect_to(:controller =>"gift", :action => "index")
     else
       flash[:notice] = "Invalid username/password combination."
       redirect_to(:action => "login")
